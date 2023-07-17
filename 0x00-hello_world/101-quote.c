7c37adee8a6c6fbb6242591d8964348839bde286
celinemwj <celinemaoudji10@gmail.com>
2023-07-17 16:01:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 1 on succes
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return(1);
+}
