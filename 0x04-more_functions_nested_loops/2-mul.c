e1960db6651bc41390ae0c0c6098c1ca00bb3fc9
celinemwj <celinemaoudji10@gmail.com>
2023-07-21 12:12:10 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,13 @@
-#include"main.h"
+#include "main.h"
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ * mul - checks for checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
  */
-
-int _isdigit(int c)
+int mul(int a, int b)
 {
-if (c >= '0' && c <= '9')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+int mul;
+mul = a * b;
+return (mul);
 }
