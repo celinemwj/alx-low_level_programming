e53933fe36cb51394e8db729770af180944a04a6
celinemwj <celinemaoudji0@gmail.com>
2023-09-28 19:04:56 -0700

    0x08. C - Recursion mandatory

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (real_prime(n, n - 1));
+}
+
+/**
+ * real_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int real_prime(int n, int m)
+{
+	if (m == 1)
+		return (1);
+	if (n % m == 0 && m > 0)
+		return (0);
+	return (real_prime(n, i - 1));
+}
